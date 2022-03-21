17c96e46571a31f99c8ab53f56b7b935cc09e072
Maria Desteffani <maria.desteffani@stud.unibas.ch>
2022-03-21 12:56:37 +0100

    made several changes to how login/logout works 1. Game/Game.java: Hashmap now uses usernames as keys. The server will now use a client's username (instead of their ip) to check if they visit for the first time or not. 2. Server/ClientHandler.java: before connecting the user to the server the client will be asked to enter a name with which the user will be created (method: askUsername). The clients name will be remembered, if he logs in again. TO DO: at the moment 2+ clients can be active as the same user by entering the same name -> needs to be fixed!

--- src/main/java/Server/ClientHandler.java
+++ src/main/java/Server/ClientHandler.java
@@ -38,7 +38,14 @@ public class ClientHandler implements Runnable {
   public void run() {
 
     // Identifies the new Client
-    user = game.connect(socket.getInetAddress(), this, socket.getInetAddress().getHostName());
+    String username = null;
+    try {
+      username = askUsername(); // asks client to enter name
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    // user = game.connect(socket.getInetAddress(), this, socket.getInetAddress().getHostName());
+    user = game.connect(socket.getInetAddress(), this, username);
     welcomeUser();
 
     try {
@@ -79,17 +86,34 @@ public class ClientHandler implements Runnable {
     }
   }
 
+  /**
+   * Server asks for clients name when joining
+   */
+  public String askUsername() throws IOException {
+    // Server asks for name and sets it as a username
+    try {
+      out.writeUTF("Please enter your name: ");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    String username = null;
+    try {
+      username = in.readUTF();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    // implementation needed so that two clients can't be logged in as the same user
+    return username;
+
+  }
+
 
   /**
    *
    */
   private void welcomeUser() {
     try {
-      // Server asks for name and sets it as a username
-      out.writeUTF("Please enter your name: ");
-      String username = in.readUTF();
-      user.setUsername(username);
-
       if (user.isFirstTime()) {
         System.out.println(user.getUsername() + " from district " + user.getDistrict() + " has connected");
         out.writeUTF("Your name was drawn at the reaping. Welcome to the Student Games, " + user.getUsername() + " from district " + user.getDistrict() + "!");
@@ -142,12 +166,5 @@ public class ClientHandler implements Runnable {
       }
   }
 
-  /**
-   * broadcast method for chat
-   * method sends a message to all clients, but not the one who sent it
-   */
-  public void broadcastChatMessage(String msg) throws IOException {
-      // TO DO
-  }
 
 }
