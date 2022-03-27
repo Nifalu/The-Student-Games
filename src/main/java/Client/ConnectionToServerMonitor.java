8224c0136701c16eef7ca4e1a67560beafec8aa9
Nico Bachmann <nico.bachmann@stud.unibas.ch>
2022-03-27 21:42:40 +0200

    Quickfix changed timer for "yes" to 15s

--- src/main/java/Client/ConnectionToServerMonitor.java
+++ src/main/java/Client/ConnectionToServerMonitor.java
@@ -17,7 +17,7 @@ public class ConnectionToServerMonitor implements Runnable{
   @Override
   public void run() {
     try {
-      Thread.sleep(10000);
+      Thread.sleep(15000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
