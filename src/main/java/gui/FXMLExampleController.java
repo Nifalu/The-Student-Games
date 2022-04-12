f35a6a9f9a2dbe7bbeba2a7afe1fdff87001c464
Maria Desteffani <maria.desteffani@stud.unibas.ch>
2022-04-12 10:42:14 +0200

    the chat now has new functions - whisper chat works now - users can change their name with /nick NewName in the chat - added a working quit button

--- src/main/java/gui/FXMLExampleController.java
+++ src/main/java/gui/FXMLExampleController.java
@@ -24,10 +24,33 @@ public class FXMLExampleController implements Initializable {
   private TextArea chat;
 
 
+    /**
+     * method reads input from the Textfield and checks, which command to send to the server
+     * if there's no command at the start of the message, it will be sent as a chat (which is the main use for this GUI)
+     * @param event
+     */
+
   @FXML
   void sendChatMessage(ActionEvent event) {
     String msg = (chatTextField.getText());
-    sendToServer.send(CommandsToServer.CHAT, msg);
+
+    if (msg.startsWith("/nick")) {
+        System.out.println("momentan msg: " + msg );
+        String[] split = msg.split(" ", 2);
+        String newName = split[1];
+        System.out.println("neui dengs: " + newName );
+        sendToServer.send(CommandsToServer.NICK, newName);
+    } else if (msg.startsWith("/whisper")) {
+        String[] split = msg.split(" ", 3);
+        if (split.length > 1) {
+            String whisperMsg = split[1] + "-" + split[2];
+            sendToServer.send(CommandsToServer.WHISPER, whisperMsg);
+        } else {
+            chat.appendText("You cannot whisper nothing!");
+        }
+    } else {
+        sendToServer.send(CommandsToServer.CHAT, msg);
+    }
     chatTextField.clear();
   }
 
@@ -50,5 +73,10 @@ public class FXMLExampleController implements Initializable {
     waitForChatThread.setName("GuiWaitForChatThread"); // set name of thread
     waitForChatThread.start(); // start thread
   }
+
+    public void quitGame(ActionEvent actionEvent) {
+        sendToServer.send(CommandsToServer.QUIT, msg);
+        chat.appendText("You left the game.");
+    }
 }
 
