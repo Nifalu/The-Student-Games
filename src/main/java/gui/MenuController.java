53eb103575cc4b9dc094339bbd48e81245ee193c
Andrina Geller <andrina.geller@stud.unibas.ch>
2022-04-28 09:01:04 +0200

    added lobbylist with the join and create Lobby feature

--- src/main/java/gui/MenuController.java
+++ src/main/java/gui/MenuController.java
@@ -75,6 +75,9 @@ public class MenuController implements Initializable {
     @FXML
     private TextField createLobbyTextField;
 
+    @FXML
+    private Label selectedLobbyLabel;
+
 
 
 
@@ -139,7 +142,8 @@ public class MenuController implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // hier ein Thread und dann StringSplitted
+        selectedLobbyLabel.setText(lobbyListView.getSelectionModel().getSelectedItem());
+
 
 
         Thread lobbyListThread = new Thread(() -> {
@@ -235,5 +239,33 @@ public class MenuController implements Initializable {
     private static String removeNewline(String str) {
 
         return str.replace("\n", "").replace("\r", "");
+    }
+    public String listViewSelectedLobby() {
+        return lobbyListView.getSelectionModel().getSelectedItem();
+    }
+
+    public void joinSelectedLobby(ActionEvent actionEvent) {
+        String selectedLobby = listViewSelectedLobby();
+        if (selectedLobby == null) {
+            selectedLobbyLabel.setText("Please select a lobby.");
+        } else {
+            String lobbyNumber = selectedLobby.substring(0, 1);
+            try {
+                Integer.parseInt(lobbyNumber);
+                sendToServer.send(CommandsToServer.CHANGELOBBY, lobbyNumber);
+                selectedLobbyLabel.setText("You are now member of Lobby: " + selectedLobby);
+            } catch (Exception e) {
+                selectedLobbyLabel.setText("Lobby needs to be open.");
+            }
+        }
+    }
+
+    public void createLobby(ActionEvent actionEvent) {
+        refreshLobbies(actionEvent);
+        String lobbyName = createLobbyTextField.getText();
+        sendToServer.send(CommandsToServer.CREATELOBBY, lobbyName);
+        sendToServer.send(CommandsToServer.PRINTLOBBIES, "");
+        refreshLobbies(actionEvent);
+        createLobbyTextField.clear();
     }
 }
