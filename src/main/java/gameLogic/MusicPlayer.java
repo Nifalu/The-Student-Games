9de9af0903f071d3dbf6cb3e6268c4c79684d90f
Vladimir Buser <vladimir.buser@stud.unibas.ch>
2022-05-18 10:08:40 +0200

    added new MusicPlayer

---
+++ src/main/java/gameLogic/MusicPlayer.java
@@ -0,0 +1,23 @@
+package gameLogic;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+/**
+ * MusicPlayer plays the given music from folder resources
+ */
+public class MusicPlayer {
+    public void play(String music) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(music).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+}
